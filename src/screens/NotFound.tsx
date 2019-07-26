23d542033b2638947c5b7cb0196a3907352fa388
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-07-26 13:38:55 +0800

    Add api handler

--- src/screens/NotFound.tsx
+++ src/screens/NotFound.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 
 const NotFound: React.FC = (): ReactElement => {
-  return <h1>404 | NOT FOUND</h1>;
+  return <h1>404 | PAGE NOT FOUND</h1>;
 };
 
 export default NotFound;
---
+++ src/utils/api.ts
@@ -0,0 +1,71 @@
+import axios, {
+  AxiosRequestConfig,
+  AxiosResponse,
+  AxiosError,
+  AxiosInstance,
+} from 'axios';
+import * as HttpStatusCode from 'http-status-codes';
+import * as storage from 'store';
+
+export interface ResponseError {
+  message: string;
+  statusCode?: number;
+}
+
+export interface ErrorResponse {
+  error: ResponseError;
+}
+
+export type ApiResponse = AxiosResponse & ErrorResponse;
+
+const setApi = (): AxiosInstance => {
+  const ApiHandler = axios.create({
+    baseURL: `localhost:8080/api`,
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  ApiHandler.interceptors.request.use(
+    (config: AxiosRequestConfig): AxiosRequestConfig => {
+      const headers = config.headers;
+      const bearer = storage.get('bearer');
+
+      if (!!bearer) {
+        config.headers = {
+          ...headers,
+          Authorization: `bearer ${bearer}`,
+        };
+      }
+      return config;
+    },
+    (err: AxiosError): ErrorResponse => {
+      Promise.reject(err);
+      return { error: { message: 'Something went wrong' } };
+    }
+  );
+
+  ApiHandler.interceptors.response.use(
+    (res: AxiosResponse): AxiosResponse => res,
+    (err: AxiosError): ErrorResponse => {
+      let error: ResponseError;
+
+      if (err.response) {
+        error = err.response.data;
+      } else if (err.request) {
+        error = {
+          message: 'Network error',
+          statusCode: HttpStatusCode.SERVICE_UNAVAILABLE,
+        };
+      } else {
+        error = { message: 'Something went wrong' };
+      }
+      Promise.reject(error);
+      return { error };
+    }
+  );
+
+  return ApiHandler;
+};
+
+const API = setApi();
+
+export default API;
