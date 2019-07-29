499c1a3e17144a01de47bb883f1bd4e7540796a3
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-07-29 14:45:39 +0800

    add middleware in API

--- mockApi/server.js
+++ mockApi/server.js
@@ -2,9 +2,16 @@ const jsonServer = require('json-server');
 const server = jsonServer.create({
   noCors: true,
 });
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./mockApi/db.json');
 const middlewares = jsonServer.defaults();
 
+server.use((req, res, next) => {
+  // res.sendStatus(401);
+  // console.log('TCL: next', next);
+  // console.log('TCL: res', res);
+  // console.log('TCL: req', req);
+  next();
+});
 server.use(middlewares);
 server.use(router);
 server.listen(3030, () => {
